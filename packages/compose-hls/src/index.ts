427cb239cb5c761daca7da850074d14cc1c3ab36
Jason Bulson <3855668+jbulson@users.noreply.github.com>
2026-02-23 23:22:10 -0700

    fix(compose-hls): export transferToS3 and update version (#36)
    This commit exports the `transferToS3` function and `TransferToS3Config`
    type from basic.js to make them available in the public API. It also bumps
    the package version to 0.0.1-experimental.5.
    
    Fixes #36

--- packages/compose-hls/src/index.ts
+++ packages/compose-hls/src/index.ts
@@ -1,2 +1,3 @@
 export { ComposeHlsClient, type ComposeHlsClientConfig } from './client.js';
 export { ComposeStorage } from './storage.js';
+export { transferToS3, type TransferToS3Config } from './basic.js';
